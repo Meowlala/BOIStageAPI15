ed20f88a18aeeca589d01d0036c8c12d3235c9a8
Michael Cohen <budjmt@aol.com>
2019-12-15 11:54:46 -0500

    fix mistaken change for roomtest file

--- basementrenovator/roomTest.lua
+++ basementrenovator/roomTest.lua
@@ -1,10 +1,2 @@
 
-return {
-	METADATA=nil,
-	{TYPE=1, VARIANT=0, SUBTYPE=0, NAME="New Room", DIFFICULTY=1, WEIGHT=1, WIDTH=13, HEIGHT=7, SHAPE=1, METADATA=nil,
-		{ISDOOR=true, GRIDX=6, GRIDY=7, SLOT=3, EXISTS=true},
-		{ISDOOR=true, GRIDX=13, GRIDY=3, SLOT=2, EXISTS=true},
-		{ISDOOR=true, GRIDX=-1, GRIDY=3, SLOT=0, EXISTS=true},
-		{ISDOOR=true, GRIDX=6, GRIDY=-1, SLOT=1, EXISTS=true},
-	},
-}
+return false
