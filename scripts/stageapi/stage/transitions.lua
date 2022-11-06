bdac1dc6b4814a3530e3c8a5baac3d21f96ef64c
OpenSauce04 <opensauce04@gmail.com>
2022-11-06 17:12:16 +0000

    Moved extra portrait location to precisely the correct position

--- scripts/stageapi/stage/transitions.lua
+++ scripts/stageapi/stage/transitions.lua
@@ -95,7 +95,7 @@ mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
         StageAPI.TransitionIsPlaying = true
         StageAPI.RenderBlackScreen()
         StageAPI.TransitionAnimation:Render(StageAPI.GetScreenCenterPosition(), Vector.Zero, Vector.Zero)
-        StageAPI.ExtraPortrait:Render(StageAPI.GetScreenCenterPosition() + Vector(-74 + StageAPI.ShakeOffset, -17)--[[Magic trial and error numbers to make it work without adjusting anm2 files]], Vector.Zero, Vector.Zero)
+        StageAPI.ExtraPortrait:Render(StageAPI.GetScreenCenterPosition() + Vector(-72 + StageAPI.ShakeOffset, -19)--[[Magic trial and error numbers to make it work without adjusting anm2 files]], Vector.Zero, Vector.Zero)
     elseif StageAPI.TransitionIsPlaying then -- Finished transition
         StageAPI.TransitionIsPlaying = false
         if StageAPI.CurrentStage then
