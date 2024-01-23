3ec1e02cca93b37caaafda048af6440bb7f4fe84
filloax <filloax@gmail.com>
2024-01-23 16:46:09 +0100

    repentogon nil checks

--- scripts/stageapi/room/roomHandler.lua
+++ scripts/stageapi/room/roomHandler.lua
@@ -1184,8 +1184,10 @@ function StageAPI.GetCurrentRoomID()
     local levelMap = StageAPI.GetCurrentLevelMap()
     if levelMap and StageAPI.CurrentLevelMapRoomID then
         return levelMap:GetRoomData(StageAPI.CurrentLevelMapRoomID).RoomID
-    else
+    elseif shared.Level then
         return StageAPI.GetCurrentListIndex()
+    else -- level not init (early-triggering repentogon callbacks)
+        error("StageAPI: tried accessing room ID before level init", 2)
     end
 end
 
@@ -1296,6 +1298,10 @@ function StageAPI.SetCurrentRoom(room)
 end
 
 function StageAPI.GetCurrentRoom()
+    -- level not init (early repentogon callbacks)
+    if not shared.Level then
+        return nil
+    end
     return StageAPI.GetLevelRoom(StageAPI.GetCurrentRoomID())
 end
 
